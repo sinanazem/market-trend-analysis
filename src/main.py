7ae75f5d7d2ae747134b65139fbd34e9728b6cf0
sinanazem <sinanaazem@gmail.com>
2023-01-14 20:54:31 +0330

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,25 @@
 import pandas as pd
-# from crawlers.blackrock import BlackRockInsights
 from crawlers.vanguard import VanguardInsights
+from crawlers.goldmansachs import GoldmanSachsInsights
 from utils import DataBaseClass
 
-# BlackRock
-# obj = BlackRockInsights('https://www.blackrock.com/us/individual/insights')
-# blackrock_insight_list = obj.get_article_insight()
-# blackrock_df = pd.DataFrame(blackrock_insight_list)
 
 
-# Vanguard
-vanguard_insight_obj = VanguardInsights('https://advisors.vanguard.com/insights/all')
-vanguard_insight_list = vanguard_insight_obj.get_article_insight()
-vanguard_df = pd.DataFrame(vanguard_insight_list)
+if __name__ == "__main__":
 
-# Store data
+    # Vanguard
+    vanguard_insight_obj = VanguardInsights('https://advisors.vanguard.com/insights/all')
+    vanguard_insight_list = vanguard_insight_obj.get_article_insight()
+    vanguard_df = pd.DataFrame(vanguard_insight_list)
 
-obj_data_base_class = DataBaseClass('postgres', 'sina2000', '0.0.0.0', 5432, 'insights_db')
-obj_data_base_class.store_data(vanguard_df, sql_table_name='insights_data')
+    #GoldmanSachs
+    obj = GoldmanSachsInsights()
+    goldman_df = obj.get_df_insights()
+
+
+    # Store data in db
+
+    obj_data_base_class = DataBaseClass('guest', 'Aa12345', 'localhost', 5432, 'insights_db')
+    obj_data_base_class.store_data(vanguard_df, sql_table_name='insights_data')
+    obj_data_base_class.store_data(goldman_df, sql_table_name='insights_data')
+
